c0e1f87565007ec5d33113d3be8995b5fba877e5
test12121245 <162971463+test12121245@users.noreply.github.com>
2024-07-06 21:53:34 +0200

    from v0.11.0 zip

--- controllers/argocd/util.go
+++ controllers/argocd/util.go
@@ -200,35 +200,27 @@ func getArgoContainerImage(cr *argoproj.ArgoCD) string {
 
 // getRepoServerContainerImage will return the container image for the Repo server.
 //
-// There are four possible options for configuring the image, and this is the
+// There are three possible options for configuring the image, and this is the
 // order of preference.
 //
 // 1. from the Spec, the spec.repo field has an image and version to use for
 // generating an image reference.
-// 2. from the Spec, the spec.version field has an image and version to use for
-// generating an image reference
-// 3. from the Environment, this looks for the `ARGOCD_IMAGE` field and uses
+// 2. from the Environment, this looks for the `ARGOCD_REPOSERVER_IMAGE` field and uses
 // that if the spec is not configured.
-// 4. the default is configured in common.ArgoCDDefaultArgoVersion and
-// common.ArgoCDDefaultArgoImage.
+// 3. the default is configured in common.ArgoCDDefaultRepoServerVersion and
+// common.ArgoCDDefaultRepoServerImage.
 func getRepoServerContainerImage(cr *argoproj.ArgoCD) string {
 	defaultImg, defaultTag := false, false
 	img := cr.Spec.Repo.Image
 	if img == "" {
-		img = cr.Spec.Image
-		if img == "" {
-			img = common.ArgoCDDefaultArgoImage
-			defaultImg = true
-		}
+		img = common.ArgoCDDefaultArgoImage
+		defaultImg = true
 	}
 
 	tag := cr.Spec.Repo.Version
 	if tag == "" {
-		tag = cr.Spec.Version
-		if tag == "" {
-			tag = common.ArgoCDDefaultArgoVersion
-			defaultTag = true
-		}
+		tag = common.ArgoCDDefaultArgoVersion
+		defaultTag = true
 	}
 	if e := os.Getenv(common.ArgoCDImageEnvName); e != "" && (defaultTag && defaultImg) {
 		return e
